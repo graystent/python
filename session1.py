147714f734225b230111a6f1b53cb910715ebd45
graystent <graystent@gmail.com>
2019-12-29 18:34:12 +0000

    Uncommented questions

--- session1.py
+++ session1.py
@@ -1,32 +1,32 @@
 # section a
-# print("hello, world")
-# print("1,2,3,4,5")
-# print("hello\nworld")
-# print("my list of names\n\talice\n\tbob\n\tcharlie")
+print("hello, world")
+print("1,2,3,4,5")
+print("hello\nworld")
+print("my list of names\n\talice\n\tbob\n\tcharlie")
 
 # section b
-# answer = (1+2)
-# print(answer)
-# print(len("hello, world"))
+answer = (1+2)
+print(answer)
+print(len("hello, world"))
 
 # section c
 # question 1
-# width = int(input("what is the width of the rectangle? "))
-# height = int(input("what is the height of the rectangle? "))
-# area = (width * height)
-# print("Rectangle has an area of " + str(area))
+width = int(input("what is the width of the rectangle? "))
+height = int(input("what is the height of the rectangle? "))
+area = (width * height)
+print("Rectangle has an area of " + str(area))
 
 # question 2
-# print(len("python"))
+print(len("python"))
 
-# word = input("give me a word ")
-# print(len(word))
+word = input("give me a word ")
+print(len(word))
 
 # question 3
-# word = ("python")
-# print(word[3], word[5])
+word = ("python")
+print(word[3], word[5])
 
-# question 6
+# question 4
 name = input("what is your name? ")
 age = int(input("how old are you?"))
 print("hello", + name, "you are currently", + str(age) "years old")
